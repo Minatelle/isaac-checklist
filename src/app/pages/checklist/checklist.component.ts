5d64fdb0ca049148dc2a31bc38d8284d28b8d06c
Minatelle <gminatelle@gmail.com>
2025-04-16 08:57:09 -0300

    refactor: checklist table

--- src/app/pages/checklist/checklist.component.ts
+++ src/app/pages/checklist/checklist.component.ts
@@ -1,10 +1,18 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import checklistData from './checklist-data.json';
 
 @Component({
   selector: 'app-checklist',
+  imports: [NgFor, NgIf],
   templateUrl: './checklist.component.html',
-  styleUrl: './checklist.component.scss'
+  styleUrl: './checklist.component.scss',
 })
 export class ChecklistComponent {
+  public characters = checklistData.characters;
+  public achievements = checklistData.achievements;
 
+  public getExtraCells(achievementsLength: number): number {
+    return this.characters.length - achievementsLength;
+  }
 }
