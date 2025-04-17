b9e59df9e996d7fdbb2343c94317fba724708953
Minatelle <gminatelle@gmail.com>
2025-04-17 15:21:23 -0300

    feat: lock and unlock achievements

--- src/app/pages/checklist/checklist.component.ts
+++ src/app/pages/checklist/checklist.component.ts
@@ -1,18 +1,40 @@
-import { NgFor, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { NgClass, NgFor, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import checklistData from './checklist-data.json';
 
 @Component({
   selector: 'app-checklist',
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf, NgClass],
   templateUrl: './checklist.component.html',
   styleUrl: './checklist.component.scss'
 })
-export class ChecklistComponent {
+export class ChecklistComponent implements OnInit {
   public characters = checklistData.characters;
   public achievements = checklistData.achievements;
+  private unlockedAchievements: string[] = [];
+
+  ngOnInit(): void {
+    const saved = localStorage.getItem('unlockedAchievements');
+    if (saved) {
+      this.unlockedAchievements = JSON.parse(saved);
+    }
+  }
 
   public getExtraCells(achievementsLength: number): number {
     return this.characters.length - achievementsLength;
+  }
+
+  public isUnlocked(achievementName: string): boolean {
+    return this.unlockedAchievements.includes(achievementName);
+  }
+
+  public lockUnlock(achievementName: string): void {
+    if (!this.isUnlocked(achievementName)) {
+      this.unlockedAchievements.push(achievementName);
+    } else {
+      const removeIndex = this.unlockedAchievements.indexOf(achievementName);
+      this.unlockedAchievements.splice(removeIndex, 1);
+    }
+    localStorage.setItem('unlockedAchievements', JSON.stringify(this.unlockedAchievements));
   }
 }
